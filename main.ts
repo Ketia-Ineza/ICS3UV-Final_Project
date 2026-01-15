13504bb0782c53696df72f79a57481d0abf7dfed
default <ketia.ineza@stu.ocsb.ca>
2026-01-15 23:09:56 +0000

    Made progress on the task

---
+++ main.ts
@@ -0,0 +1,14 @@
+// Simple To-Do List Program
+
+var tasks = ["Math homework", "English reading", "Science study"];
+var completed = [false, false, false];
+
+console.log("My To-Do List:");
+
+for (var i = 0; i < tasks.length; i++) {
+  if (completed[i] == true) {
+    console.log((i + 1) + ". " + tasks[i] + " - Completed");
+  } else {
+    console.log((i + 1) + ". " + tasks[i] + " - Not Completed");
+  }
+}
