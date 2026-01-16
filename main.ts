28dd8d259ce1b30fb551eb0ac1e046741a9dda1b
default <ketia.ineza@stu.ocsb.ca>
2026-01-16 21:51:21 +0000

    Student To-Do List program

--- main.ts
+++ main.ts
@@ -1,14 +1,13 @@
 // Student To-Do List Program
 
-var tasks = ["Math homework", "English reading", "Science study"];
-var completed = [false, false, false];
+const tasks: string[] = ["Math homework", "English reading", "Science study"];
+const completed: boolean[] = [false, false, false];
 
 console.log("My To-Do List:");
 
-for (var i = 0; i < tasks.length; i++) {
-  if (completed[i] == true) {
+for (let i = 0; i < tasks.length; i++)
+  if (completed[i] === true) {
     console.log((i + 1) + ". " + tasks[i] + " - Completed");
   } else {
     console.log((i + 1) + ". " + tasks[i] + " - Not Completed");
   }
-}
